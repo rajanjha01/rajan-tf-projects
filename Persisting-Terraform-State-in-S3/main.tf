c6d2fce99fa9c7ce929f094b60f484db91204364
Rajan.R.Jha@shell.com <Rajan.R.Jha@shell.com>
2022-11-08 12:48:11 +0530

    my project code

---
+++ Persisting-Terraform-State-in-S3/main.tf
@@ -0,0 +1,22 @@
+provider "docker" {
+  host = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx-image" {
+  name = "nginx"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx-image.latest
+  name  = "nginx"
+  ports {
+    internal = 80
+    external = var.external_port
+    protocol = "tcp"
+  }
+}
+
+output "url" {
+  description = "Browser URL for container site"
+  value       = join(":", ["http://localhost", tostring(var.external_port)])
+}
